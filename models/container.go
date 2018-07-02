c98e1526c7aa7b08bdc33aa4c57a36688c2216ff
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-07-02 20:49:07 +0900

    added image struct

--- models/container.go
+++ models/container.go
@@ -6,10 +6,15 @@ import (
 
 type Container struct {
 	Id int64 `xorm:"not null BIGINT pk autoincr 'id'"`
-	ImageId int64 `xorm:"image_id"`
-	Created time.Time `xorm:"created"`
+	ImageId int64 `xorm:"image_id not null"`
+	Created time.Time `xorm:"created not null"`
 	Ipv4 string `xorm:"ipv4_address"`
-	Cpu int8 `xorm:"cpu"`
-	Memory int32 `xorm:"memory"`
-	Disk int64 `xorm:"disk"`
+	Cpu int8 `xorm:"cpu not null"`
+	Memory int32 `xorm:"memory not null"`
+	Disk int64 `xorm:"disk not null"`
+}
+
+type Image struct {
+	Id int64 `xorm:"not null BIGINT pk autoincr 'id'"`
+	Name string `xorm:"image_name not null"`
 }
