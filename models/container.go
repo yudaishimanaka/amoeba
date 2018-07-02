fa80fbb91e3b978c280589b2db6983ab62189c39
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-07-02 20:36:24 +0900

    added models.go

--- models/container.go
+++ models/container.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"time"
+)
+
+type Container struct {
+	Id int64 `xorm:"not null BIGINT pk autoincr 'id'"`
+	ImageId int64 `xorm:"image_id"`
+	Created time.Time `xorm:"created"`
+	Ipv4 string `xorm:"ipv4_address"`
+	Cpu int8 `xorm:"cpu"`
+	Memory int32 `xorm:"memory"`
+	Disk int64 `xorm:"disk"`
+}
