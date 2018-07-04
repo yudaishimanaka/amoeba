35afc81170f3c927a20b8bde5668ee191c40c517
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-07-04 17:41:32 +0900

    update controller

--- controller.go
+++ controller.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// API for Container
 func fetchAllContainer(c *gin.Context){
 	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Fetch all containers successfully."})
 }
@@ -22,5 +23,18 @@ func removeContainer(c *gin.Context){
 }
 
 func updateContainer(c *gin.Context){
+
+}
+
+// API for Images
+func fetchImageList(c *gin.Context){
+
+}
+
+func createImage(c *gin.Context){
+
+}
+
+func updateImage(c *gin.Context){
 
 }
