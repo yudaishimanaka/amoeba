6db2a4451089fc9edeb91e3726c88b3f2319c595
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-07-04 16:43:25 +0900

    added controller.go

---
+++ controller.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func fetchAllContainer(c *gin.Context){
+
+}
+
+func fetchSingleContainer(c *gin.Context){
+
+}
+
+func createContainer(c *gin.Context){
+
+}
+
+func removeContainer(c *gin.Context){
+
+}
+
+func updateContainer(c *gin.Context){
+
+}
