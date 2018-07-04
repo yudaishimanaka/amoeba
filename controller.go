6f2bf53e2e02c7319747d8697e6a60e150f7f26f
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-07-04 17:13:56 +0900

    fixed controller.go

--- controller.go
+++ controller.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func fetchAllContainer(c *gin.Context){
-
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Fetch all containers successfully."})
 }
 
 func fetchSingleContainer(c *gin.Context){
