4d4b6860a3f690919315ded74249073f5a0a01ff
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-08-13 18:51:15 +0900

    fixed models/container.go

--- models/container.go
+++ models/container.go
@@ -7,6 +7,7 @@ import (
 type Container struct {
 	Id int64 `xorm:"not null BIGINT pk autoincr 'id'"`
 	ImageId int64 `xorm:"image_id not null"`
+	OsType string `xorm:"os_type not null"`
 	Created time.Time `xorm:"created not null"`
 	Ipv4 string `xorm:"ipv4_address"`
 	Cpu int8 `xorm:"cpu not null"`
